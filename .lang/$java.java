91153ec92c29f3bb808873ae47372be1eb1ac5b7
AP-Studios <61108504+AP-Studios@users.noreply.github.com>
2020-12-18 10:28:02 +0100

    Update $java.java

--- .lang/$java.java
+++ .lang/$java.java
@@ -1,3 +1,21 @@
-/*
-
-*/
+public class default {
+    public static void main() {
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+        default(null);
+    }
+}
